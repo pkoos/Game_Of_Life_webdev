ab807fe4e4e84eea289869638d95a690ae197992
Paul Koos <paully_k5298@yahoo.com>
2023-10-25 11:59:03 -0700

    initial commit.

---
+++ README.md
@@ -0,0 +1,36 @@
+# Conway's Game of Life
+
+This is written in very basic html5 and Javascript. The purpose
+of this project was mainly for me to do a little bit of webdev
+practice, as well as play around with the game of life. This is
+written using the html5 canvas, as well as vanilla Javascript.
+There are no JS libraries at all, and everything is in one file.
+Once I get the thing working, I might try to convert the code to
+TypeScript, but we'll see.
+
+The way I run this program is using the basic Python http server.
+With Python installed, you can run the following command:
+
+```
+python3 -m http.server
+```
+
+In some cases, you do not need the '3' above, and would run this command:
+
+```
+python -m http.server
+```
+
+This is a first pass, with mostly naive implementations, so there are many areas to improve upon.
+
+Possible improvements:
+
+1. Unglobal a lot of things, like arrays, contexts, etc.
+2. Add persistence. Currently when you refresh the page, you lose everything.
+3. Write in something other than Javascript. If I can figure this out, I might try to do this in `wasm` or another language.
+4. Use some type of restful functions.
+5. Add a web framework to make the website better.
+
+I can never remember this, so it's in the readme :)
+
+[Markdown Guide](https://www.markdownguide.org/basic-syntax/)
---
+++ gameoflife.js
@@ -0,0 +1,168 @@
+class Pixel {
+    constructor(context, height, width, isAlive = false) {
+        this.context = context;
+        this.height = height * PIXEL_SIZE;
+        this.width = width * PIXEL_SIZE;
+        this.isAlive = isAlive;
+    }
+
+    drawPixel() {
+        this.context.fillRect(this.width, this.height, PIXEL_SIZE, PIXEL_SIZE);
+    }
+
+    clearPixel() {
+        this.context.clearRect(this.width, this.height, PIXEL_SIZE, PIXEL_SIZE);
+    }
+
+    togglePixel() {
+        if(this.isAlive) {
+            this.drawPixel();
+        }
+        else {
+            this.clearPixel();
+            this.context.strokeRect(this.width, this.height, PIXEL_SIZE, PIXEL_SIZE);
+        }
+    }
+
+    bothAreEven() {
+        return this.height % 2 == 0 && this.width % 2 == 0;
+    }
+    
+    bothAreOdd() {
+        return this.height % 2 == 1 && this.width % 2 == 1;
+    }
+
+    onlyHeightEven() {
+        return this.height % 2 == 0 && this.width % 2 == 1;
+    }
+
+    onlyWidthEven() {
+        return this.height % 2 == 1 && this.width % 2 == 0;
+    }
+}
+
+function drawGrid(context) {
+    var vert_lines_counter = 0;
+
+    while (vert_lines_counter < MAX_WIDTH) {
+        vert_lines_counter += PIXEL_SIZE;
+        context.moveTo(vert_lines_counter, 0);
+        context.lineTo(vert_lines_counter, MAX_HEIGHT);
+        context.stroke();
+    }
+    
+    var horiz_lines_counter = 0;
+    
+    while (horiz_lines_counter < MAX_HEIGHT) {
+        horiz_lines_counter += PIXEL_SIZE;
+        context.moveTo(0, horiz_lines_counter);
+        context.lineTo(MAX_WIDTH, horiz_lines_counter);
+        context.stroke();
+    }
+}
+
+function getNeighborPixels() {
+    console.log("getNeighborPixels");
+}
+
+function initPixels(context) {
+    for(var h = 0;h < HEIGHT_PIXELS; h++) {
+        for(var w = 0; w < WIDTH_PIXELS; w++) {
+            var pixel = new Pixel(context, h, w);
+            pixels.push(pixel);
+        }
+    }
+}
+
+function clearPixels() {
+    pixels.forEach((pixel) => {
+        pixel.isAlive = false;
+        pixel.togglePixel();
+    });
+}
+
+function drawSquares() {
+    pixels.forEach((pixel) => {
+        if (pixel.bothAreEven() || pixel.bothAreOdd()) {
+            pixel.isAlive = true;
+            pixel.togglePixel();
+        }
+    });
+}
+
+function switchSquares() {
+    pixels.forEach((pixel) => {
+        if(pixel.onlyWidthEven() || pixel.onlyHeightEven()) {
+            pixel.isAlive = true;
+            pixel.togglePixel();
+        }
+    });
+}
+// *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+// The code that we run is below this line, above this line are all the functions needed.
+var MAX_WIDTH = 1200;
+var MAX_HEIGHT = 800;
+var PIXEL_SIZE = 25;
+var WIDTH_PIXELS = MAX_WIDTH / PIXEL_SIZE;
+var HEIGHT_PIXELS = MAX_HEIGHT / PIXEL_SIZE;
+
+var TOTAL_PIXELS = (MAX_WIDTH/PIXEL_SIZE) * (MAX_HEIGHT/PIXEL_SIZE)
+
+var pixels = [];
+
+// var grid = document.getElementById("CGoL_Grid");
+// var g_ctx = grid.getContext("2d");
+var c = document.getElementById("CGoL_Board");
+var ctx = c.getContext("2d");
+
+initPixels(ctx);
+drawGrid(ctx);
+
+var drawButton = document.getElementById("drawButton");
+drawButton.addEventListener("click", (event) => {
+    console.log("Draw button was clicked");
+    clearPixels();
+    drawGrid(ctx);
+    drawSquares();
+});
+
+var switchButton = document.getElementById("switchButton");
+switchButton.addEventListener("click", (event) => {
+    console.log("Switch button was clicked");
+    clearPixels(ctx);
+    drawGrid(ctx);
+    switchSquares(ctx);
+});
+
+var clearButton = document.getElementById("clearButton");
+clearButton.addEventListener("click", (event) => {
+    clearPixels(ctx);
+    drawGrid(ctx);
+});
+
+c.addEventListener("click", (event) => {
+
+    var canvas_x = event.clientX - c.offsetLeft;
+    var canvas_y = event.clientY - c.offsetTop;
+    
+    console.log(`normalized coordinates: x: ${canvas_x}, y: ${canvas_y}`);
+
+
+    var x_element = Math.floor(canvas_x / PIXEL_SIZE);
+    var y_element = Math.floor(canvas_y / PIXEL_SIZE);
+
+    console.log(`x pixel: ${x_element}, y pixel: ${y_element}`);
+
+    var pixelIndex = y_element * WIDTH_PIXELS + x_element;
+    var pixel = pixels[pixelIndex];
+
+    console.log(pixel);
+    
+    pixel.isAlive = !pixel.isAlive;
+    console.log(`is pixel alive? ${pixel.isAlive}`);
+    pixel.togglePixel();
+});
+
+console.log(`Total Pixels: ${TOTAL_PIXELS}, Height: ${HEIGHT_PIXELS}, Width: ${WIDTH_PIXELS}`);
+console.log("pixels:");
+console.log(pixels);
