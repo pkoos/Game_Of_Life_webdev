0c68405bc4aede33e52713850f3656cad4c132c0
Paul Koos <paully_k5298@yahoo.com>
2023-10-25 17:30:52 -0700

    refactor Pixel more, remove unneeded instance variables.

--- src/js/pixel.js
+++ src/js/pixel.js
@@ -4,33 +4,28 @@ import {WIDTH_PIXELS, PIXEL_SIZE, } from "./constants.js";
 class Pixel {
     constructor(context, y, x, isAlive = false) {
         this.context = context;
-        this.x = x;
         this.y = y;
-        this.index = y * WIDTH_PIXELS + x;
-        this.canvas_y = y * PIXEL_SIZE;
-        this.canvas_x = x * PIXEL_SIZE;
+        this.x = x;
         this.isAlive = isAlive;
     }
 
     toggle() {
         if(this.isAlive) {
-            this.context.fillRect(this.canvas_x, this.canvas_y, PIXEL_SIZE, PIXEL_SIZE);
+            this.context.fillRect(this.x * PIXEL_SIZE, this.y * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE);
         }
         else {
-            this.context.clearRect(this.canvas_x, this.canvas_y, PIXEL_SIZE, PIXEL_SIZE);
-            this.context.strokeRect(this.canvas_x, this.canvas_y, PIXEL_SIZE, PIXEL_SIZE);
+            this.context.clearRect(this.x * PIXEL_SIZE, this.y * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE);
+            this.context.strokeRect(this.x * PIXEL_SIZE, this.y * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE);
         }
     }
 
     bothSame() {
-        return this.canvas_x % 2 === this.canvas_y % 2;
+        return (this.x * PIXEL_SIZE) % 2 === (this.y * PIXEL_SIZE) % 2;
     }
 
     bothDifferent() {
-        return this.canvas_x % 2 !== this.canvas_y % 2;
+        return (this.x * PIXEL_SIZE) % 2 !== (this.y * PIXEL_SIZE) % 2;
     }
-
-
 }
 
 export { Pixel };
